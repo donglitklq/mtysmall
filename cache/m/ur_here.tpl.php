ede0f9587670646afefb1d2a476ee7808bdbb054
donglitklq <379502693@qq.com>
2018-04-24 13:51:17 +0800

    .

---
+++ cache/m/ur_here.tpl.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.26, created on 2018-04-24 09:16:29
+         compiled from inc/ur_here.tpl */ ?>
+<div class="urHere"><?php echo $this->_tpl_vars['lang']['ur_here']; ?>
+：<?php echo $this->_tpl_vars['ur_here']; ?>
+</div>
